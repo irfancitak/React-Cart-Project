3005fcc03f1ecea66eacb19eb78369f850e220b0
İrfan Çıtak <irfancitak6@gmail.com>
2022-08-29 20:13:10 +0300

    React-E-Commerce-Cart-Project
    React-E-Commerce-Cart-Project

---
+++ src/GlobalComponents/ThemeProvider.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState, useEffect, useContext } from "react";
+
+const ThemeContext = createContext();
+
+const ThemeProvider = (props) => {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || false
+  );
+
+  useEffect(() => {}, [theme]);
+  localStorage.setItem("theme", JSON.stringify(theme));
+
+  const setThemeMode = (mode) => setTheme(mode);
+  return (
+    <ThemeContext.Provider value={{ theme, setThemeMode }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
+
+const useThemeHook = () => {
+  const { theme } = useContext(ThemeContext);
+  return [theme];
+};
+
+export { ThemeProvider, ThemeContext, useThemeHook };
